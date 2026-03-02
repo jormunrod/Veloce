cfcbc3f03ff6a92af673e176767b70cea8c42ad5
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2026-03-02 17:35:51 +0100

    Initial Commit

---
+++ Veloce/VeloceApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloceApp.swift
+//  Veloce
+//
+//  Created by Jorge Muñoz Rodríguez on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct VeloceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
